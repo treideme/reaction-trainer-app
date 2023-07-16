2113c57b17aa5b6b6419d5f9051418af0c60de6f
Thomas Reidemeister <treideme@gmail.com>
2023-07-16 17:39:16 -0400

    WiP

--- app/src/main/java/com/reidemeister/reactiontrainer/PodsManager.java
+++ app/src/main/java/com/reidemeister/reactiontrainer/PodsManager.java
@@ -3,14 +3,112 @@ package com.reidemeister.reactiontrainer;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.util.Log;
+import androidx.core.app.ActivityCompat;
 
 import java.util.HashMap;
 import java.util.Map;
+
+class Pod extends BluetoothGattCallback {
+    private BluetoothDevice device;
+    private Context context;
+
+    @SuppressLint("MissingPermission")
+    public Pod(BluetoothDevice device, Context context) {
+        this.device = device;
+        this.context = context;
+        device.connectGatt(context, true, this);
+    }
+
+    // Handlers for BluetoothGatt and LeScan events.
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+        super.onConnectionStateChange(gatt, status, newState);
+        if (newState == BluetoothGatt.STATE_CONNECTED) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                if (!gatt.discoverServices()) {
+                    // Error starting service discovery.
+                    connectFailure();
+                }
+            }
+            else {
+                // Error connecting to device.
+                connectFailure();
+            }
+        }
+        else if (newState == BluetoothGatt.STATE_DISCONNECTED) {
+            // Disconnected, notify callbacks of disconnection.
+            rx = null;
+            tx = null;
+            notifyOnDisconnected(this);
+        }
+    }
+
+    @Override
+    public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+        super.onServicesDiscovered(gatt, status);
+        // Notify connection failure if service discovery failed.
+        if (status == BluetoothGatt.GATT_FAILURE) {
+            connectFailure();
+            return;
+        }
+        gatt.getServices()
+
+        // Save reference to each UART characteristic.
+        tx = gatt.getService(UART_UUID).getCharacteristic(TX_UUID);
+        rx = gatt.getService(UART_UUID).getCharacteristic(RX_UUID);
+
+        // Save reference to each DIS characteristic.
+        disManuf = gatt.getService(DIS_UUID).getCharacteristic(DIS_MANUF_UUID);
+        disModel = gatt.getService(DIS_UUID).getCharacteristic(DIS_MODEL_UUID);
+        disHWRev = gatt.getService(DIS_UUID).getCharacteristic(DIS_HWREV_UUID);
+        disSWRev = gatt.getService(DIS_UUID).getCharacteristic(DIS_SWREV_UUID);
+
+        // Add device information characteristics to the read queue
+        // These need to be queued because we have to wait for the response to the first
+        // read request before a second one can be processed (which makes you wonder why they
+        // implemented this with async logic to begin with???)
+        readQueue.offer(disManuf);
+        readQueue.offer(disModel);
+        readQueue.offer(disHWRev);
+        readQueue.offer(disSWRev);
+
+        // Request a dummy read to get the device information queue going
+        gatt.readCharacteristic(disManuf);
+
+        // Setup notifications on RX characteristic changes (i.e. data received).
+        // First call setCharacteristicNotification to enable notification.
+        if (!gatt.setCharacteristicNotification(rx, true)) {
+            // Stop if the characteristic notification setup failed.
+            connectFailure();
+            return;
+        }
+        // Next update the RX characteristic's client descriptor to enable notifications.
+        BluetoothGattDescriptor desc = rx.getDescriptor(CLIENT_UUID);
+        if (desc == null) {
+            // Stop if the RX characteristic has no client descriptor.
+            connectFailure();
+            return;
+        }
+        desc.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+        if (!gatt.writeDescriptor(desc)) {
+            // Stop if the client descriptor could not be written.
+            connectFailure();
+            return;
+        }
+        // Notify of connection completion.
+        notifyOnConnected(this);
+    }
+
+}
 
 public class PodsManager {
     private MainActivity parent;
@@ -36,7 +134,6 @@ public class PodsManager {
                     // FIXME: Update
                 } else {
                     knownDevices.put(address, device);
-                    device.connectGatt(context, true, this);
                     Log.d("PodsManager", "added (" + address + ", " + name + ") = " + result.toString());
                 }
             }
